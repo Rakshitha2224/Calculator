569791ddc1a3d1e0532fb40751ad8301a5373bc3
Vaishnavi Bachhav <vaishnavi.bachhav111@gmail.com>
2025-09-10 09:48:10 -0400

    Implement addition with loop-style sum

--- Calculator.java
+++ Calculator.java
@@ -23,7 +23,10 @@ public class Calculator
 
     public int addition(int x, int y) 
     { 
-       //return x + y; 
+        int sum = 0;
+        sum += x;
+        sum += y;
+        return sum;
     } 
     public int subtraction(int x, int y) 
     { 
